f776dc1aa8d86fd90cb845278c61becfa3358f1c
khannsameer <sk8717108@gmail.com>
2025-08-20 23:51:45 +0530

    refactor(about): move About section out of (users) group into app/about/

---
+++ app/about/page.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1>About Page</h1>
+      <button>
+        <Link href="/">Home</Link>
+      </button>
+    </>
+  );
+};
+
+export default About;
