c3f0cf9564934afa658d78355fb50d7b8705ba53
khannsameer <sk8717108@gmail.com>
2025-08-22 21:35:58 +0530

    feat(layout): improve users layout with new changes

--- app/(users)/layout.js
+++ app/(users)/layout.js
@@ -1,10 +1,20 @@
 import "../globals.css";
 import Navbar from "@/app/components/Navbar";
+import { Roboto } from "next/font/google";
+import { Vollkorn } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+});
+
+const vollkorn = Vollkorn({
+  subsets: ["latin"],
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body>
+    <html lang="en">
+      <body className={vollkorn.className}>
         {/* <h1>Home layout</h1> */}
         <Navbar />
         {children}
