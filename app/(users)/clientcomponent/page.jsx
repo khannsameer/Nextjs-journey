4344f79a9a9c38bf6fb4470798ad163d8f95f5b2
khannsameer <sk8717108@gmail.com>
2025-08-26 21:51:09 +0530

    Fix: Render post data properly in ClientComponent

--- app/(users)/clientcomponent/page.jsx
+++ app/(users)/clientcomponent/page.jsx
@@ -1,6 +1,22 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+const URL = "https://jsonplaceholder.typicode.com/posts";
+
 const ClientComponent = () => {
+  const [postData, setPostData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(URL);
+      const data = await res.json();
+      console.log(data);
+      setPostData(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <h1>Client Component</h1>
@@ -10,6 +26,21 @@ const ClientComponent = () => {
       >
         Click Me
       </button>
+
+      <ul className="grid grid-cols-3 gap-5 mt-3">
+        {postData.map((post) => (
+          <li
+            key={post.id}
+            className="border p-3 rounded-lg shadow-sm hover:shadow-md"
+          >
+            <h2 className="font-bold text-lg">{post.title}</h2>
+            <p className="text-sm text-gray-600">{post.body}</p>
+            <small className="block text-gray-500">
+              User ID: {post.userId}
+            </small>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
