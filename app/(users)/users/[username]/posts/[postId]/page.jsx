c3eb03d125709a0636976df59c9e31ac27b18921
khannsameer <sk8717108@gmail.com>
2025-08-27 22:11:20 +0530

    Update nested dynamic route page for username and postId

--- app/(users)/users/[username]/posts/[postId]/page.jsx
+++ app/(users)/users/[username]/posts/[postId]/page.jsx
@@ -1,5 +1,29 @@
-const SingleProfilePost = async (props) => {
-  const user = await props.params;
+"use client";
+
+import { use } from "react";
+
+//this is for server component
+
+// const SingleProfilePost = async (props) => {
+//   const user = await props.params;
+//   console.log(user);
+
+//   return (
+//     <>
+//       <h1>Hello Dynamic Routes</h1>
+//       <h2>
+//         User: {user.username}, PostId: {user.postId}{" "}
+//       </h2>
+//     </>
+//   );
+// };
+
+// export default SingleProfilePost;
+
+//dynamic routes in client components using react use API
+
+const SingleProfilePost = (props) => {
+  const user = use(props.params);
   console.log(user);
 
   return (
