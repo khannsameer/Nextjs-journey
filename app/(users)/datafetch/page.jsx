831f2828027430a4ee2e64a008ea659cbc8339fa
khannsameer <sk8717108@gmail.com>
2025-08-28 23:29:16 +0530

    Add datafetch and servercomp pages

---
+++ app/(users)/datafetch/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DataFetch = async () => {
+  return (
+    <>
+      <h1>Data Fetching</h1>
+    </>
+  );
+};
+
+export default DataFetch;
---
+++ app/(users)/datafetch/servercomp/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DataFetchServer = async () => {
+  return (
+    <>
+      <h1>DataFetchServer</h1>
+    </>
+  );
+};
+
+export default DataFetchServer;
