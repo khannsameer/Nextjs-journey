ebd974f17b1229a71da8d78610530ca9330f4ded
khannsameer <sk8717108@gmail.com>
2025-08-28 22:53:14 +0530

    Add catch-all blog route page

---
+++ app/(users)/blog/[...slug]/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Blog = async (props) => {
+  const { slug } = await props.params;
+  console.log(slug);
+
+  return (
+    <>
+      <h1>Blog</h1>
+    </>
+  );
+};
+
+export default Blog;
